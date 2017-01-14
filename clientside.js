aec94377693ee521d8d69645cac7e1f74df07e8c
Maakep <schon.fredrik@gmail.com>
2017-01-14 16:14:00 +0100

    Working embroyo

---
+++ clientside.js
@@ -0,0 +1,60 @@
+var socket = io();
+		
+$(document).ready(function(){
+	$(window).keyup(function(e){
+		if (e.keyCode === 0 || e.keyCode === 32) {
+			e.preventDefault();
+			socket.emit("next turn");
+	    } else if (e.keyCode === 27) {	    	
+	    	socket.emit("pause game");
+	    	$('.pause-overlay').toggle();
+	    }
+	});
+
+	$('#add-player').click(function(){
+		var name = $('#player-name').val();
+		socket.emit("add player", name);
+		$('#player-name').val('');
+	});
+
+	$('#start-game').click(function(){
+		socket.emit("start game");
+		$(this).hide();
+	});
+});
+
+socket.on('update state', function(state){
+	var html = "";
+	for (var key in state.players) {
+		var turnColor = "";
+		if(state.turn && state.players[key].id == state.turn.id){
+			turnColor = "background-color: rgb(103, 255, 103)";
+		}
+
+		var date = new Date(null);
+		date.setSeconds(state.players[key].time);
+		var timestamp = date.toISOString().substr(11, 8);
+
+		var timeLast = state.players[key].lastTime;
+		if (timeLast > 60){
+			var minutes = Math.floor(timeLast / 60);
+			var seconds = timeLast % 60;
+			timeLast = minutes + "m " + seconds;
+		}
+		timeLast += "s ";
+
+		html += '<div style="' + turnColor + '" class="player" id=' + state.players[key].id + '><div class="name">' + state.players[key].name + '</div><div class="timer">' + timestamp + '</div><div class="last-round">+ ' + timeLast + '</div></div>';
+	}
+
+	$('.amount').text(state.spectators);
+	$('.player-board').html(html);
+	
+});
+
+socket.on('update timer', function(player){
+	$('#'+player.id).text(player.timer);
+});
+
+socket.on('admin', function(){
+	$('.admin-controls').toggle();
+});
---
+++ index.js
@@ -0,0 +1,172 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var players = {};
+var state = {};
+var i = 0;
+var admin = "null";
+var totalTime = 0;
+var ticker;
+var gamePaused = false;
+var gameStarted = false;
+
+io.on('connection', function(socket){
+	setAdmin(socket);
+
+	updateState();
+
+	socket.on('disconnect', function(){
+		if (admin === socket.id){
+			admin = "null";
+		}
+		updateState();
+	});
+
+	socket.on('add player', function(name){
+		createNewPlayer(name, socket.id);
+	});
+
+	socket.on('next turn', function(){
+		if (/*admin === socket.id &&*/ !state.paused && gameStarted){
+			nextTurn();
+		}
+	});
+
+	socket.on('start game', function(){
+		if (admin === socket.id){
+			startGame();
+		}
+	});
+
+	socket.on('pause game', function(){
+		if (admin === socket.id){
+			togglePauseGame();
+		}
+	});
+});
+
+
+function getPlayerFromId(id){
+	for (var key in players) {
+		if(players[key].id === id){
+			return players[key];
+		}
+	}
+}
+
+function getPlayersLength(){
+	return Object.keys(players).length;
+}
+
+function createNewPlayer(name, id) {
+	if (admin === id){
+		var player = {
+			'id': i,
+			'name': name,
+			'myturn': false,
+			'time': 0,
+			'lastTime': 0
+		};
+		players[player.id] = player;
+
+		updateState();
+		i++;
+	}
+}
+
+
+function updateState() {
+	state = {
+		'players': players,
+		'spectators': getSpectators(),
+		'turn': getTurn(),
+		'totalTime': totalTime,
+		'paused': gamePaused
+	};
+
+	io.emit('update state', state);
+}
+
+function getSpectators(){
+	return Object.keys(io.sockets.sockets).length;
+}
+
+
+function getTurn() {
+	for (var key in players) {
+		if(players[key].myturn == true) {
+			return players[key];
+		}
+	}
+	return players[0];
+}
+
+function nextTurn() {
+
+	var currPlayer = getTurn();
+	currPlayer.myturn = false;
+	currPlayer.time += currPlayer.lastTime;
+
+	if ((currPlayer.id + 1) >= getPlayersLength()){
+		players[0].myturn = true;
+		players[0].lastTime = 0;
+	} else {
+		players[currPlayer.id+1].myturn = true;
+		players[currPlayer.id+1].lastTime = 0;
+	}
+	updateState();
+}
+
+function setAdmin(socket) {
+	if(admin == "null") {
+		admin = socket.id;
+		socket.emit('admin');
+		console.log("New admin");
+	}
+}
+
+function startGame() {
+	if(!gameStarted) {
+		gameStarted = true;
+		players[0].myturn = true;
+		updateState();
+		ticker = setInterval(tick, 1000);
+	}
+}
+
+function tick(){
+	if(state.paused == false){
+		players[state.turn.id].lastTime += 1;
+		totalTime += 1;
+		updateState();
+	}
+}
+
+
+function togglePauseGame() {
+	gamePaused = !gamePaused;
+	updateState();
+}
+
+
+
+
+
+
+
+http.listen(8080, function() {
+  console.log('listening on *:8080');
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/clientside.js', function(req, res){
+  res.sendFile(__dirname + '/clientside.js');
+});
+
+app.get('/style', function(req, res){
+  res.sendFile(__dirname + '/style.css');
+});
