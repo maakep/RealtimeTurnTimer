9d299d2b03538ee99a12ac6c88108a0c3508de27
Maakep <schon.fredrik@gmail.com>
2019-06-08 03:29:04 +0200

    Modernize project a bit

--- clientside.js
+++ clientside.js
@@ -1,49 +1,62 @@
 var socket = io();
-		
-$(document).ready(function(){
-	$(window).keyup(function(e){
+
+$(document).ready(function () {
+	$(window).keyup(function (e) {
 		if (e.keyCode === 0 || e.keyCode === 32) {
 			e.preventDefault();
 			socket.emit("next turn");
-	    } else if (e.keyCode === 27) {	    	
-	    	socket.emit("pause game");
-	    }
+		} else if (e.keyCode === 27) {
+			socket.emit("pause game");
+		}
 	});
 
-	$('#add-player').click(function(){
+	$('#add-player').click(function () {
 		var name = $('#player-name').val();
 		socket.emit("add player", name);
 		$('#player-name').val('');
 	});
 
-	$('#start-game').click(function(){
+	$('#start-game').click(function () {
 		socket.emit("start game");
-		$(this).hide();
+	});
+
+	$('#end-game').click(function () {
+		socket.emit("end game");
+	});
+
+	$('#all-admin').click(function () {
+		socket.emit("all admin");
 	});
 });
 
-socket.on('update state', function(state){
+socket.on('update state', function (state) {
 	var html = "";
 	for (var key in state.players) {
-		var turnColor = "";
-		if(state.turn && state.players[key].id == state.turn.id){
-			turnColor = "background-color: rgb(103, 255, 103)";
-		}
+		var myTurn = state.turn && state.players[key].id == state.turn.id;
 
 		var date = new Date(null);
 		date.setSeconds(state.players[key].time);
 		var timestamp = date.toISOString().substr(11, 8);
 
 		var timeLast = state.players[key].lastTime;
-		if (timeLast > 60){
+		if (timeLast > 60) {
 			var minutes = Math.floor(timeLast / 60);
 			var seconds = timeLast % 60;
 			timeLast = minutes + "m " + seconds;
 		}
 		timeLast += "s ";
 
-		html += '<div style="' + turnColor + '" class="player" id=' + state.players[key].id + '><div class="name">' + state.players[key].name + '</div><div class="timer">' + timestamp + '</div><div class="last-round">+ ' + timeLast + '</div></div>';
+		html += '<div class="' + (myTurn ? "my-turn " : "") + 'player" id=' + state.players[key].id + '><div class="name">' + state.players[key].name + '</div><div class="timer">' + timestamp + '</div><div class="last-round">+ ' + timeLast + '</div></div>';
 	}
+
+	var isRealAdmin = socket.id == state.admin;
+	$('.admin-controls').toggle(state.allAdmin || isRealAdmin);
+
+	$('#all-admin').text('All admin: ' + state.allAdmin);
+	$('#all-admin').prop('disabled', !isRealAdmin);
+	$('#start-game').toggle(!state.gameStarted);
+	$('#end-game').toggle(state.gameStarted);
+	$('.pause-overlay').toggle(state.gamePaused);
 
 	$('.amount').text(state.spectators);
 
@@ -53,18 +66,13 @@ socket.on('update state', function(state){
 
 	$('.total-game-time').text(totalTimestamp);
 	$('.player-board').html(html);
-	
+
 });
 
-socket.on('update timer', function(player){
-	$('#'+player.id).text(player.timer);
+socket.on('update timer', function (player) {
+	$('#' + player.id).text(player.timer);
 });
 
-socket.on('admin', function(){
-	$('.admin-controls').toggle();
-});
-
-
-socket.on('pause game', function(){
-	$('.pause-overlay').toggle();
+socket.on('admin', function (isAdmin) {
+	$('.admin-controls').toggle(isAdmin);
 });
--- index.js
+++ index.js
@@ -4,66 +4,78 @@ var io = require('socket.io')(http);
 
 var players = {};
 var state = {};
-var i = 0;
 var admin = "null";
 var totalTime = 0;
 var ticker;
 var gamePaused = false;
 var gameStarted = false;
+var allAdmin = false;
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 	setAdmin(socket);
-
 	updateState();
 
-	socket.on('disconnect', function(){
-		if (admin === socket.id){
+	socket.on('disconnect', function () {
+		if (admin === socket.id) {
 			admin = "null";
 		}
 		updateState();
 	});
 
-	socket.on('add player', function(name){
-		createNewPlayer(name, socket.id);
+	socket.on('add player', function (name) {
+		createNewPlayer(name, socket);
 	});
 
-	socket.on('next turn', function(){
-		if (/*admin === socket.id &&*/ !state.paused && gameStarted){
+	socket.on('next turn', function () {
+		if (isAuthorized(socket) && !state.gamePaused && gameStarted) {
 			nextTurn();
 		}
 	});
 
-	socket.on('start game', function(){
-		if (admin === socket.id){
+	socket.on('start game', function () {
+		if (isAuthorized(socket))
 			startGame();
+	});
+
+	socket.on('end game', function () {
+		if (isAuthorized(socket))
+			endGame();
+	});
+
+	socket.on('pause game', function () {
+		if (isAuthorized(socket)) {
+			togglePauseGame();
 		}
 	});
 
-	socket.on('pause game', function(){
-		if (admin === socket.id){
-			togglePauseGame();
-			io.emit('pause game');
+	socket.on('all admin', function () {
+		if (admin === socket.id) {
+			allAdmin = !allAdmin;
+			updateState();
 		}
 	});
 });
 
+function isAuthorized(socket) {
+	return allAdmin || admin == socket.id;
+}
 
-function getPlayerFromId(id){
+function getPlayerFromId(id) {
 	for (var key in players) {
-		if(players[key].id === id){
+		if (players[key].id === id) {
 			return players[key];
 		}
 	}
 }
 
-function getPlayersLength(){
+function getPlayersLength() {
 	return Object.keys(players).length;
 }
 
-function createNewPlayer(name, id) {
-	if (admin === id){
+function createNewPlayer(name, socket) {
+	if (isAuthorized(socket)) {
 		var player = {
-			'id': i,
+			'id': getPlayersLength(),
 			'name': name,
 			'myturn': false,
 			'time': 0,
@@ -72,7 +84,6 @@ function createNewPlayer(name, id) {
 		players[player.id] = player;
 
 		updateState();
-		i++;
 	}
 }
 
@@ -83,20 +94,23 @@ function updateState() {
 		'spectators': getSpectators(),
 		'turn': getTurn(),
 		'totalTime': totalTime,
-		'paused': gamePaused
+		'gamePaused': gamePaused,
+		'allAdmin': allAdmin,
+		'admin': admin,
+		'gameStarted': gameStarted,
 	};
 
 	io.emit('update state', state);
 }
 
-function getSpectators(){
+function getSpectators() {
 	return Object.keys(io.sockets.sockets).length;
 }
 
 
 function getTurn() {
 	for (var key in players) {
-		if(players[key].myturn == true) {
+		if (players[key].myturn == true) {
 			return players[key];
 		}
 	}
@@ -109,25 +123,27 @@ function nextTurn() {
 	currPlayer.myturn = false;
 	currPlayer.time += currPlayer.lastTime;
 
-	if ((currPlayer.id + 1) >= getPlayersLength()){
+	if ((currPlayer.id + 1) >= getPlayersLength()) {
 		players[0].myturn = true;
 		players[0].lastTime = 0;
 	} else {
-		players[currPlayer.id+1].myturn = true;
-		players[currPlayer.id+1].lastTime = 0;
+		players[currPlayer.id + 1].myturn = true;
+		players[currPlayer.id + 1].lastTime = 0;
 	}
 	updateState();
 }
 
 function setAdmin(socket) {
-	if(admin == "null") {
-		admin = socket.id;
-		socket.emit('admin');
+	if (admin == "null" || isAuthorized(socket)) {
+		if (admin == "null")
+			admin = socket.id;
+
+		socket.emit('admin', admin == socket.id);
 	}
 }
 
 function startGame() {
-	if(!gameStarted) {
+	if (!gameStarted) {
 		gameStarted = true;
 		players[0].myturn = true;
 		updateState();
@@ -135,8 +151,19 @@ function startGame() {
 	}
 }
 
-function tick(){
-	if(state.paused == false){
+function endGame() {
+	if (gameStarted) {
+		gameStarted = false;
+		players = {};
+		totalTime = 0;
+		i = 0;
+		clearInterval(ticker);
+		updateState();
+	}
+}
+
+function tick() {
+	if (state.gamePaused == false) {
 		players[state.turn.id].lastTime += 1;
 		totalTime += 1;
 		updateState();
@@ -145,8 +172,10 @@ function tick(){
 
 
 function togglePauseGame() {
-	gamePaused = !gamePaused;
-	updateState();
+	if (gameStarted) {
+		gamePaused = !gamePaused;
+		updateState();
+	}
 }
 
 
@@ -154,19 +183,19 @@ function togglePauseGame() {
 
 
 
-
-http.listen(8080, function() {
-  console.log('listening on *:8080');
+var port = process.argv[2] || 8080;
+http.listen(port, function () {
+	console.log('listening on *:' + port);
 });
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/clientside.js', function(req, res){
-  res.sendFile(__dirname + '/clientside.js');
+app.get('/clientside.js', function (req, res) {
+	res.sendFile(__dirname + '/clientside.js');
 });
 
-app.get('/style', function(req, res){
-  res.sendFile(__dirname + '/style.css');
+app.get('/style', function (req, res) {
+	res.sendFile(__dirname + '/style.css');
 });
